2a0994410ff9ed79073c4e914dad8386fcaef058
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-05-01 22:28:49 -0700

    taxicab.py

--- taxicab.py
+++ taxicab.py
@@ -6,23 +6,23 @@ class Taxicab:
     """Represent a Taxicab with x and y coordinates and an odometer reading."""
     def __init__(self, x, y):
         """Initialize the Taxicab with x and y coordinates and sets the odometer to zero."""
-        self._x_coordinate = x
-        self._y_coordinate = y
-        self._odormeter = 0
+        self._x_coord = x
+        self._y_coord = y
+        self._odometer = 0
     def get_x_coord(self):
         """Return the x coordinate of the taxicab."""
-        return self._x_coordinate
+        return self._x_coord
     def get_y_coord(self):
         """Return the y coordinate of the taxicab."""
-        return self._y_coordinate
+        return self._y_coord
     def get_odormeter(self):
         """Return the odometer of the taxicab."""
-        return self._odormeter
+        return self._odometer
     def move_x(self, distance):
         """Move the taxicab horizontally by the given distance."""
-        self._x_coordinate += distance
-        self._odormeter += abs(distance)
+        self._x_coord += distance
+        self._odometer += abs(distance)
     def move_y(self, distance):
         """Move the taxicab vertically by the given distance."""
-        self._y_coordinate += distance
-        self._odormeter += abs(distance)
+        self._y_coord += distance
+        self._odometer += abs(distance)
